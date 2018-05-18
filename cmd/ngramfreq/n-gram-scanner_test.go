d9e7fb786a9a05339c17b27e75bf9f402c527245
Aaron Longwell <a@adl.io>
2018-05-18 10:08:56 -0700

    Adds tests for scanning files from disk.

--- cmd/ngramfreq/n-gram-scanner_test.go
+++ cmd/ngramfreq/n-gram-scanner_test.go
@@ -1,24 +1,57 @@
 package main
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 )
+
+func TestSonnets(t *testing.T) {
+	s := NewScanner(3)
+	err := s.ScanFile("./testdata/sonnets.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	s.Score()
+
+	assertScannerOutputPrefix(t, s, 100, "3 - thy self thy\n3 - why dost thou")
+}
+
+func TestScanNonexistentFile(t *testing.T) {
+	s := NewScanner(3)
+	err := s.ScanFile("./testdata/fakefile.txt")
+	if err == nil {
+		t.Error("./testdata/fakefile.txt doesn't exist. ScanFile() should have thrown an error about that.")
+	}
+}
 
 func TestEmptyString(t *testing.T) {
 	s := NewScanner(3)
 	r := strings.NewReader("")
 	s.Scan(r)
+	s.Score()
 
 	if len(s.Grams) != 0 {
-		t.Error("An empty string shouldn't have any ngrams in it.")
+		t.Error("An empty string shouldn't have any ngrams.")
 	}
+
+	if len(s.Freqs) != 0 {
+		t.Error("An empty string shouldn't have any ngrams.")
+	}
+
+	b := bytes.Buffer{}
+	s.WriteTopN(100, &b)
+	if b.String() != "" {
+		t.Errorf("Empty input should result in empty output. Got: %v", b)
+	}
+
 }
 
 func Test3WordString(t *testing.T) {
 	s := NewScanner(3)
 	r := strings.NewReader("my first trigram")
 	s.Scan(r)
+	s.Score()
 
 	ngf, _ := s.Grams["my first trigram"]
 	if ngf.Freq != 1 {
@@ -29,12 +62,15 @@ func Test3WordString(t *testing.T) {
 	if found {
 		t.Errorf("Grams contained something that wasn't a trigram.")
 	}
+
+	assertScannerOutputPrefix(t, s, 100, "1 - my first trigram")
 }
 
 func TestPunctuationOnlyString(t *testing.T) {
 	s := NewScanner(3)
 	r := strings.NewReader("lorem ipsum dolor ... --- ... --- ... --- ... ---")
 	s.Scan(r)
+	s.Score()
 
 	ngf, _ := s.Grams["lorem ipsum dolor"]
 	if ngf.Freq != 1 {
@@ -45,18 +81,22 @@ func TestPunctuationOnlyString(t *testing.T) {
 	if found {
 		t.Errorf("All-spaces n-gram found '%s'", ngf)
 	}
+
+	assertScannerOutputPrefix(t, s, 100, "1 - lorem ipsum dolor")
 }
 
 func TestPunctuationCleanup(t *testing.T) {
 	s := NewScanner(3)
 	r := strings.NewReader(`
-			I love
+			It's important that I explain that I
+			love
 			sandwiches. Very much.
 			Lorem ipsum dolor sit amet. Chapter 10
 			I love sandwiches. (I LOVE SANDWICHES!!).
 			East Side Deli makes the best sandwiches with love.
 			`)
 	s.Scan(r)
+	s.Score()
 
 	ngf, ok := s.Grams["i love sandwiches"]
 	if ok != true {
@@ -65,6 +105,8 @@ func TestPunctuationCleanup(t *testing.T) {
 	if ngf.Freq != 3 {
 		t.Errorf("Expected 'i love sandwiches' freq of 3. Got %d", ngf.Freq)
 	}
+
+	assertScannerOutputPrefix(t, s, 100, "3 - i love sandwiches")
 }
 
 func TestScrubWhenClean(t *testing.T) {
@@ -78,5 +120,13 @@ func TestScrubWordLowerCasing(t *testing.T) {
 	s := ScrubWord("CleanWord")
 	if s != "cleanword" {
 		t.Errorf("Expected 'cleanword', got '%s'.", s)
+	}
+}
+
+func assertScannerOutputPrefix(t *testing.T, s *Scanner, n int, prefix string) {
+	b := bytes.Buffer{}
+	s.WriteTopN(n, &b)
+	if !strings.HasPrefix(b.String(), prefix) {
+		t.Errorf("Expected output to start with\n\n%s\n\nActual output:\n%s", prefix, b.String())
 	}
 }
